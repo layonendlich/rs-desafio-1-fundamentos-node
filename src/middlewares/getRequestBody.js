6bea95b307bcfcfc170763bf0c87e5ca5337f1be
Layon Endlich Rodrigues <layonendlich@hotmail.com>
2025-11-05 00:15:37 -0300

    Ajuste no recebimento do payload

--- src/middlewares/getRequestBody.js
+++ src/middlewares/getRequestBody.js
@@ -6,8 +6,29 @@ export async function getRequestBody (request, response) {
     }
 
     try {
+        let body = {}
+        let data = null
+        const contentType = request.headers['content-type'].split(';')[0].toLowerCase().trim()
+        
         const str = Buffer.concat(streamParts).toString()
-        request.body = JSON.parse(str)
+        
+        switch (contentType) {
+            case 'multipart/form-data':
+                break
+            case 'application/x-www-form-urlencoded':
+                data = str.split('&').reduce( (queryParams, param) => {
+                    const [key, value] = param.split('=')
+                    queryParams[key] = value
+                    return queryParams
+                }, {})
+                body = {...data}
+                break
+            case 'application/json':
+            default:
+                body = {...JSON.parse(str)}
+                break
+        }
+        request.body = body
     } catch {
         request.body = null
     }
